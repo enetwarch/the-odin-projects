4499d082581bf76aadfbee557b668da8e40c65f0
Enetwarch <enetwarch@protonmail.com>
2025-07-02 18:58:44 +0800

    style: added eslint + prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  tabWidth: 2,
+  printWidth: 120,
+  singleQuote: false,
+  semi: true,
+  objectWrap: "collapse",
+  trailingComma: "all",
+};
